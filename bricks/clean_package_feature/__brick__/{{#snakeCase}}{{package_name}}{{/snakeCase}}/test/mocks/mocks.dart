7f7227baf2332669ea11ab21356ccf483f79a65a
Arthur Monteiro Alves Melo <monteiroamelo@gmail.com>
2022-05-09 17:13:02 -0300

    chore: add mocks

---
+++ bricks/clean_package_feature/__brick__/{{#snakeCase}}{{package_name}}{{/snakeCase}}/test/mocks/mocks.dart
@@ -0,0 +1,4 @@
+// append and organize
+import 'package:{{package_name.snakeCase()}}/src/domain/repository/{{name.snakeCase()}}_repository.dart';
+
+class Mock{{name.pascalCase()}}Repository extends Mock implements {{name.pascalCase()}}Repository{}
